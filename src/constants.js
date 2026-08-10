3c1c756b8557ab3e96b9cff8efeff219551824fa
KristinaStefanac <kristina.stefanac123@gmail.com>
2026-08-10 15:17:25 +0200

    Dodaj osnovnu konfiguraciju backenda

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const PREDEFINED_SLOTS = [
+    '09:00-10:00',
+    '10:00-11:00',
+    '14:00-15:00',
+    '16:00-17:00',
+    '17:00-18:00',
+  ]
+  
+  export const SERVICES = ['dog walk', 'dog bath']
+  
+  export const RESERVATION_STATUSES = ['pending', 'accepted', 'rejected']
+  
+  export const REVIEW_VALIDITY_HOURS = 24
+  
